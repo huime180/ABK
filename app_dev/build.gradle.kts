ae31dc83bee77d736ec19e9809f6f8446fabbe33
xingguangcuican <xingguangcuican666@foxmail.com>
2026-05-16 03:16:10 +0800

    Update build.gradle.kts

--- app_dev/build.gradle.kts
+++ app_dev/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.abk.kernel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.6-dev"
+        versionCode = 10010
+        versionName = "1.1.0-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
